ce44b1022c10b833de82739cce1c5484b86a1dcf
JeromeMouly <mouly.jerome@gmail.com>
2018-12-07 13:21:58 +0100

    add new files and folders

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,12 @@
-// Modal Image Gallery
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-}
-// Change style of navbar on scroll
-window.onscroll = function() { myFunction() };
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navBar");
+var sticky = navbar.offsetTop;
 
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    }
-}
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
