2e4d43851112f19430270f7c55522e73bf4fec29
JeromeMouly <mouly.jerome@gmail.com>
2019-01-04 07:45:01 +0100

    Add Back to top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,23 +14,3 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
-
-/* ----- Back To Top Btn ----- */
-
-// window.onscroll = function () {
-//   scrollFunction()
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("myBtn").style.display = "block";
-//   } else {
-//     document.getElementById("myBtn").style.display = "none";
-//   }
-// }
-
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
